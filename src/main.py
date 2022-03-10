850320e5b8508aebeec3ddeac71122d3a73e1c33
root <root@localhost>
2022-03-11 01:35:55 +0500

    auto publish 1646944555423

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
-def factorial(n):
-    fact = 1
-    for i in range(1,n+1):
-           fact = fact*i
-    return fact
+def factorial (n):
+    if n <= 1 :
+        return 1
+    else:
+        return n * factorial(n-1)
 
-print(factorial(5))
+print(factorial(4))
